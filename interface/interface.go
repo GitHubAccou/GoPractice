48b87ef67fa07af248023ffe4868e7b447253c66
Easter <624867029@qq.com>
2018-10-25 17:29:28 +0800

    Update interface.go

--- interface/interface.go
+++ interface/interface.go
@@ -24,7 +24,7 @@ func main(){
 	//实现方法是指方法的receiver是类型本身，而不是类型的指针！！
 	var s Shape=Cube{4}
 	fmt.Println(s.area())
-	// var s1 Shape=Circle{3}    //如果不注释掉，该行会报错，因为Circle并没有实现Shape接口
+	// var s1 Shape=Circle{3}    // 如果不注释掉，该行会报错，因为Circle并没有实现Shape接口
 	s1:=&Circle{3}
 	fmt.Println(s1.area()) //虽然Circle 没有实现Shape接口，但是他拥有area方法
 }
