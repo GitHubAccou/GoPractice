36221c71ec10f7368eefeed8534b7b78da45ce79
Easter <624867029@qq.com>
2019-10-17 22:09:23 +0800

    test fyne event

--- test.go
+++ test.go
@@ -4,8 +4,15 @@ import (
 	"fyne.io/fyne/widget"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/theme"
+	"strconv"
 )
 
+	var inEntry=&widget.Entry{OnChanged:statistics}
+	var lenLabel=&widget.Label{}
+	var numLabel=&widget.Label{}
+	var upLabel=&widget.Label{}
+	var loLabel=&widget.Label{}
+	var outEntry=&widget.Entry{}
 func main() {
 	app := app.New()
 	tbP:=widget.NewTabContainer(
@@ -21,13 +28,13 @@ func main() {
 	w := app.NewWindow("Tools")
 	w.SetContent(
 		widget.NewVBox(
-		widget.NewGroup(`输入输出`,widget.NewHBox(widget.NewLabel("输入:"),widget.NewEntry()),widget.NewHBox(widget.NewLabel("输出:"),widget.NewEntry())),
+		widget.NewGroup(`输入输出`,widget.NewHBox(widget.NewLabel("输入:"),inEntry),widget.NewHBox(widget.NewLabel("输出:"),outEntry)),
 		widget.NewGroup(`算法`,tbP),
 		widget.NewGroup(`统计`,
-			widget.NewHBox(widget.NewLabel("字符数:"),widget.NewEntry()),
-			widget.NewHBox(widget.NewLabel("数字个数:"),widget.NewEntry()),
-			widget.NewHBox(widget.NewLabel("大写字母个数:"),widget.NewEntry()),
-			widget.NewHBox(widget.NewLabel("小写字母个数:"),widget.NewEntry()),
+			widget.NewHBox(widget.NewLabel("字符数:"),lenLabel),
+			widget.NewHBox(widget.NewLabel("数字个数:"),numLabel),
+			widget.NewHBox(widget.NewLabel("大写字母个数:"),upLabel),
+			widget.NewHBox(widget.NewLabel("小写字母个数:"),loLabel),
 		),
 	))
 	app.Settings().SetTheme(theme.LightTheme())
@@ -36,4 +43,15 @@ func main() {
 
 type DateLable struct{
 
+}
+
+
+func statistics(str string){
+	lenLabel.SetText(strconv.Itoa(len(str)+1))
+	numLabel.SetText(strconv.Itoa(len(str)+2))
+	upLabel.SetText(strconv.Itoa(len(str)+3))
+	loLabel.SetText(strconv.Itoa(len(str)*4))
+	if(len(str)<300){
+		outEntry.SetText(str)
+	}
 }
